fdce09d8cfb6efbceb43f39f320c2d04332f00d0
Vardominator <barsegyanvarderes@gmail.com>
2016-12-06 18:07:49 -0800

    finished front-end application

--- BCI_EEG_FrontEnd_WPF/BCI_EEG_FrontEnd_WPF/MainWindow.xaml.cs
+++ BCI_EEG_FrontEnd_WPF/BCI_EEG_FrontEnd_WPF/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
 
 namespace BCI_EEG_FrontEnd_WPF
 {
@@ -56,7 +57,8 @@ namespace BCI_EEG_FrontEnd_WPF
                 // Enable batchsizeSlider and ensure that the maximum value corresponds to the table height
                 batchsizeSlider.IsEnabled = true;
                 batchsizeSlider.Minimum = 0;
-                batchsizeSlider.Maximum = dataGrid.Height;
+                batchsizeSlider.Maximum = dataGridSource.Rows.Count;
+               
 
                 // Populate Feature Selectors
                 foreach (var column in dataGridSource.Columns)
@@ -68,35 +70,99 @@ namespace BCI_EEG_FrontEnd_WPF
                 // Initialize neural network creator list box
                 layerCreatorListBox.Items.Add($"{dataGridSource.Columns.Count - 1} Nodes @ Input Layer");
 
+
+                int numberOfClasses = FindNumberOfClasses(dataGridSource);
+                layerCreatorListBox.Items.Add($"{numberOfClasses} Nodes @ Output Layer");
+
+
+                removeHiddenLayerButton.IsEnabled = true;
+                addHiddenLayerButton.IsEnabled = true;
+
             }
-
-
-
-
+            
         }
 
         private void clearDataButton_Click(object sender, RoutedEventArgs e)
         {
             dataGrid.ItemsSource = null;
+
+            removeHiddenLayerButton.IsEnabled = false;
+            addHiddenLayerButton.IsEnabled = false;
+
+            horizonalAxisComboBox.Items.Clear();
+            verticalAxisComboBox.Items.Clear();
         }
 
         private void addHiddenLayerButton_Click(object sender, RoutedEventArgs e)
         {
+
             string hiddenLayerNumber = hiddenLayerNumberTextBox.Text;
             string hiddenLayerNodeCount = hiddenLayerNodeCountTextBox.Text;
-            layerCreatorListBox.Items.Add($"{hiddenLayerNodeCount} nodes @ layer {hiddenLayerNumber}");
+
+            int indexToAdd = int.Parse(hiddenLayerNumber);
+
+            if (indexToAdd > 0 && indexToAdd < layerCreatorListBox.Items.Count && !hiddenLayerNodeCountTextBox.Equals("0"))
+            {
+                layerCreatorListBox.Items.Insert(indexToAdd, $"{hiddenLayerNodeCount} Nodes @ Hidden Layer");
+            }
+
         }
 
         private void removeHiddenLayerButton_Click(object sender, RoutedEventArgs e)
         {
             int selectedItemIndex = layerCreatorListBox.SelectedIndex;
 
-            layerCreatorListBox.Items.RemoveAt(selectedItemIndex);
+            if (selectedItemIndex > 0 && selectedItemIndex < layerCreatorListBox.Items.Count - 1)
+            {
+                layerCreatorListBox.Items.RemoveAt(selectedItemIndex);
+            }
+
         }
 
         private void batchsizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             batchsizeTextBox.Text = batchsizeSlider.Value.ToString();
+        }
+
+        private void stepsSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            stepsTextBox.Text = stepsSlider.Value.ToString();
+        }
+
+        private void learningrateSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            learningrateTextBox.Text = learningrateSlider.Value.ToString("0.####");
+        }
+
+        // Neural network designer
+        private int FindNumberOfClasses(DataTable dataTable)
+        {
+            int lastColumnIndex = dataTable.Columns.Count - 1;
+            int numOfClasses = 0;
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                string valueStr = row[lastColumnIndex].ToString();
+
+                if (int.Parse(valueStr) > numOfClasses)
+                {
+                    numOfClasses = int.Parse(valueStr);
+                }
+            }
+
+            return numOfClasses + 1;
+        }
+
+        private void trainButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(frameworkComboBox.SelectedIndex == 0)
+            {
+                // Tensorflow
+            }
+            else if(frameworkComboBox.SelectedIndex == 1)
+            {
+                // SKlearn
+            }
         }
     }
 }
